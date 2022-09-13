20cb83f181a0c2f15dcfe53a7cc4865029f17773
Rocko <codingwithrock@gmail.com>
2022-09-13 12:54:47 +0600

    declaration completed

---
+++ declaration.js
@@ -0,0 +1,20 @@
+// !variable types and Deceleration.
+var x = 10 < 30 || 10 === 10;
+
+let y = 11;
+
+const z = 12;
+
+
+console.log(x);
+console.log(y);
+console.log(z);
+
+/* var can act like global variable but...
+it could be problem because it can be multiple declaration.
+
+
+let is block scope it's good and can be modified.
+
+
+const it's also block scope it's good but it can't be modified.*/
