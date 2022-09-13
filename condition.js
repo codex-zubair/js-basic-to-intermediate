7ec770e3068cf22f7d20431df62424078602a428
Rocko <codingwithrock@gmail.com>
2022-09-13 13:34:35 +0600

    codition

---
+++ condition.js
@@ -0,0 +1,19 @@
+// !Advance Condition.
+let x = 20 === 20 && 20 > 30;
+
+// if X not True.
+if(!x)
+{
+    console.log(x);
+
+    const y = x? x:true;
+
+    console.log(y);
+}
+
+else if(x === false)
+{
+    console.log("X now False");
+    // here i'll not come because one condition is done.
+}
+
