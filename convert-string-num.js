6f436dcab0c38bb292481716e873f29e49523b24
Rocko <codingwithrock@gmail.com>
2022-09-13 23:54:02 +0600

    added string

---
+++ convert-string-num.js
@@ -0,0 +1,13 @@
+string = '2000';
+num = 3000;
+
+// ! convert string to integer.
+console.log(typeof (string));
+console.log(typeof (+string));
+
+
+console.log("======")
+
+// ! convert integer to string.
+console.log(typeof (num));
+console.log(typeof (num + ''));
