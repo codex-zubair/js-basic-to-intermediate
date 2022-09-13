28976a1b30d3c369258b40697ce1e28e593d5469
Rocko <codingwithrock@gmail.com>
2022-09-13 23:39:52 +0600

    advance object access

---
+++ object-and-properties.js
@@ -0,0 +1,17 @@
+obj = {
+    name: 'rock',
+    id: 20,
+    result: [1232.2,232,43,534,5,4,],
+    obj2:{
+        name: 'wife'
+    }
+}
+
+console.log(obj);
+console.log('-----------------')
+console.log(Object.keys(obj));
+console.log('-----------------')
+console.log(Object.values(obj));
+console.log('-----------------')
+console.log(Object.entries(obj));
+console.log('-----------------')
